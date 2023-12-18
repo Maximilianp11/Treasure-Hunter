b804f5954eac014694c22286a3b480d045bfbd5e
BT_2N2_15 <sample@bths.edu>
2023-12-18 13:48:39 -0500

    Added marsh terrain and boots needed to cross a marsh

--- Town.java
+++ Town.java
@@ -124,14 +124,16 @@ public class Town {
      */
     private Terrain getNewTerrain() {
         double rnd = Math.random();
-        if (rnd < .2) {
+        if (rnd < .166) {
             return new Terrain(Colors.CYAN + "Mountains" + Colors.RESET, "Rope");
-        } else if (rnd < .4) {
+        } else if (rnd < .332) {
             return new Terrain(Colors.CYAN +"Ocean" + Colors.RESET, "Boat");
-        } else if (rnd < .6) {
+        } else if (rnd < .498) {
             return new Terrain(Colors.CYAN + "Plains" + Colors.RESET, "Horse");
-        } else if (rnd < .8) {
+        } else if (rnd < .664) {
             return new Terrain(Colors.CYAN + "Desert" + Colors.RESET, "Water");
+        } else if (rnd < .83) {
+            return new Terrain(Colors.CYAN + "Marsh" + Colors.RESET, "Boots");
         } else {
             return new Terrain(Colors.CYAN + "Jungle" + Colors.RESET, "Machete");
         }
